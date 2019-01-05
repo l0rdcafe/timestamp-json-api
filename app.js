3c82a3941050803d9bc37983272b9a0973a8a89a
Ismail Arafa <ismailarafa18@gmail.com>
2019-01-04 20:16:41 -0500

    add 404 support

--- app.js
+++ app.js
@@ -7,4 +7,8 @@ const app = express();
 app.use(morgan);
 app.use("/api/v1", apiRouter);
 
+app.use((req, res, next) => {
+  res.status(404).json({msg: "Sorry can't find that!"})
+});
+
 app.listen(process.env.PORT || 80);
